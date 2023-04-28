5d04f1a9217b92c145099e32cd64ed4cf193bfc1
Diego França <diego.franca92@gmail.com>
2023-04-28 09:19:53 -0300

    create botom bar and page view with scroll

---
+++ lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:personal_app/experiences_page.dart';
+import 'package:personal_app/profile_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int pageAtual = 0;
+  late PageController pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: pageAtual);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pc,
+        children: [ProfilePage(), ExperiencesPage()],
+        onPageChanged: setPaginaAtual,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list), label: 'Minhas Experiências')
+        ],
+        currentIndex: pageAtual,
+        onTap: (page) {
+          pc.animateToPage(page,
+              duration: Duration(milliseconds: 400), curve: Curves.ease);
+        },
+      ),
+    );
+  }
+
+  setPaginaAtual(page) {
+    setState(() {
+      pageAtual = page;
+    });
+  }
+}
